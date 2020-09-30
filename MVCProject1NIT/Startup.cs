7474cb7f54922c3829fe7c127eb74aba90c95bfc
DIVYA PRAKASH <DIVYA PRAKASH@DESKTOP-21TP6H0>
2020-09-30 07:33:48 +0530

    Add project files.

---
+++ MVCProject1NIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCProject1NIT.Startup))]
+namespace MVCProject1NIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
